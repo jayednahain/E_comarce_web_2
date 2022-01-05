997eb9cfeac836e264b14dc82581fe09cc1dd8a9
jayednahian <jayednahian@yahoo.com>
2022-01-05 11:59:22 +0600

    complete_pagiantion

--- store_app/urls.py
+++ store_app/urls.py
@@ -5,6 +5,6 @@ from store_app.views import store_view,product_detail
 
 urlpatterns = [
     path('',store_view,name='store_link'),
-    path('/<str:category_slug>',store_view,name='product_by_category_link'),
-    path('/Detail/<str:product_slug>',product_detail,name='product_detail_link')
+    path('Category/<str:category_slug>',store_view,name='product_by_category_link'),
+    path('Detail/<str:product_slug>',product_detail,name='product_detail_link')
 ]
--- store_app/views.py
+++ store_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,get_object_or_404
+from django.core.paginator import EmptyPage,PageNotAnInteger,Paginator
 
 from store_app.models import Product
 from store_app.models import Category
@@ -15,12 +16,31 @@ def store_view(request,category_slug = None):
       categories = get_object_or_404(Category,slug = category_slug)
       product = Product.objects.filter(category =categories,is_available=True)
       total_product = product.count()
+
+      """pagination"""
+      page_number = request.GET.get('page', None)
+      if total_product==1:
+         display_item = total_product
+      else:
+         display_item =total_product-1
+      paginator = Paginator(product, display_item)
+      display_product = paginator.get_page(page_number)
+
+
    else:
       product = Product.objects.all().filter(is_available=True)
       total_product = product.count()
 
+      """pagination"""
+      page_number = request.GET.get('page',None)
+      print(page_number)
+      #display_item=6
+      paginator = Paginator(product,6)
+      display_product = paginator.get_page(page_number)
+      
+
    contenxt = {
-      'all_product': product,
+      'all_product': display_product,
       'total_product':total_product,
       'all_category':all_category
    }
@@ -45,3 +65,4 @@ def product_detail(request,product_slug):
       'all_category': all_category
    }
    return render(request,'prodcut_detail.html',contenxt)
+
